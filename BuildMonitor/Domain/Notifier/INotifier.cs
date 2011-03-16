23054a9f6d2ec156939c6a55974b973f32b1cae9
David Adsit <david.adsit@gmail.com>
2011-03-16 01:24:57 -0600

    Removing deleted files and folders.

--- BuildMonitor/Domain/Notifier/INotifier.cs
+++
@@ -1,7 +0,0 @@
-namespace BuildMonitor.Domain.Notifier
-{
-	public interface INotifier
-	{
-		void SetBuildQuality(BuildStatus currentBuildStatus, BuildStatus previousBuildStatus, string requestorName);
-	}
-}
